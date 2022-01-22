09b8e4b7e36deee9cc79a7e12d9e748306ddfabf
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2022-01-22 20:00:57 +0900

    add : Link

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,7 +1,19 @@
 //our-domain.com/news/something-important
+import Link from "next/link";
+import {Fragment} from "react";
 
 function NewsPage() {
-  return <h1>The News Page</h1>;
+  return (
+    <Fragment>
+      <h1>The News Page</h1>
+      <ul>
+        <Link href="/news/nextjs-is-a-great-framework">
+          <li>NextJS is A Great FrameWork</li>
+        </Link>
+        <li>SomeThing Else</li>
+      </ul>
+    </Fragment>
+  );
 }
 
 export default NewsPage;
